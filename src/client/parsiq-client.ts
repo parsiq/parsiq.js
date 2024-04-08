7ba93970d9ab61a094a776404c7d5b2aaeaeb1d4
TimurRassolov <timur@parsiq.net>
2024-04-08 16:12:38 +0300

    remove unnecessary comments

--- src/client/parsiq-client.ts
+++ src/client/parsiq-client.ts
@@ -28,17 +28,17 @@ import { TransactionLifecycleHooksRequestHandler } from './transaction-lifecycle
 import { CreateTransactionLifecycle } from '../dto/transaction-lifecycle';
 
 export enum ChainId {
-  ETH_MAINNET = 'eip155-1', // Eth Mainnet
-  ETH_HOLESKY = 'eip155-17000', // Eth Goerli
-  ETH_SEPOLIA = 'eip155-11155111', // Eth Goerli
-  AVALANCHE_MAINNET = 'eip155-43114', // Avax Mainnet
-  BNB_MAINNET = 'eip155-56', // BNB Chain Mainnet
-  POLYGON_MAINNET = 'eip155-137', // Polygon Mainnet
-  ARBITRUM_NITRO_MAINNET = 'eip155-42161', // Arbitrum Mainnet
-  METIS_MAINNET = 'eip155-1088', // Metis Mainnet
-  POLYGON_ZKEVM_MAINNET = 'eip155-1101', // Polygon zkEVM Mainnet
+  ETH_MAINNET = 'eip155-1',
+  ETH_HOLESKY = 'eip155-17000',
+  ETH_SEPOLIA = 'eip155-11155111',
+  AVALANCHE_MAINNET = 'eip155-43114',
+  BNB_MAINNET = 'eip155-56',
+  POLYGON_MAINNET = 'eip155-137',
+  ARBITRUM_NITRO_MAINNET = 'eip155-42161',
+  METIS_MAINNET = 'eip155-1088',
+  POLYGON_ZKEVM_MAINNET = 'eip155-1101',
   BNB_TESTNET = 'eip155-97',
-  OPBNB_TESTNET = 'eip155-5611', // opBNB Testnet
+  OPBNB_TESTNET = 'eip155-5611',
   POLYGON_MUMBAI = 'eip155-80001',
   ARBITRUM_SEPOLIA = 'eip155-421614',
 }
